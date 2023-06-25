783336ef335b803ab094ff644dd590546da91cb8
Lovkin Valerii <vlovkin@zp.edu.ua>
2023-06-25 16:24:32 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -21,13 +21,15 @@ import blog.views
 import blango_auth.views
 from django_registration.backends.activation.views import RegistrationView
 from blango_auth.forms import BlangoRegistrationForm
+import blog.api_urls
 
 urlpatterns = [
+    path("api/v1/", include("blog.api_urls")),
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
-    #path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     #path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("allauth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
